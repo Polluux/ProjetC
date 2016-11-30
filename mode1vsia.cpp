2bd0602f3b036e66f4915ea1bb51ed4d40f9906a
Alexandre BOUDINE <E166228H@I011V7pc09.ensinfo.sciences.univ-nantes.prive>
2016-11-30 13:06:10 +0100

    plante plus quand l'IA tue 2 bateaux et il place les siens en random

--- mode1vsia.cpp
+++ mode1vsia.cpp
@@ -21,52 +21,70 @@ void Mode1vsIA::initialiser(){
 
     factory = shared_ptr<FactoryCygne>(new FactoryCygne);
     shared_ptr<Bateau> b1 = factory->creerBateau();
-    carteIA->ajouterBateau(b1);
+   // carteIA->ajouterBateau(b1);
 
     factory = shared_ptr<FactoryOie>(new FactoryOie);
     shared_ptr<Bateau> b2 = factory->creerBateau();
     b2->setY(1);
-    carteIA->ajouterBateau(b2);
+    //carteIA->ajouterBateau(b2);
 
-    factory = shared_ptr<FactoryOie>(new FactoryOie);
     shared_ptr<Bateau> b3 = factory->creerBateau();
     b3->setY(2);
-    carteIA->ajouterBateau(b3);
+   // carteIA->ajouterBateau(b3);
 
     factory = shared_ptr<FactoryCanard>(new FactoryCanard);
     shared_ptr<Bateau> b4 = factory->creerBateau();
     b4->setY(3);
-    carteIA->ajouterBateau(b4);
+    //carteIA->ajouterBateau(b4);
 
-    factory = shared_ptr<FactoryCanard>(new FactoryCanard);
     shared_ptr<Bateau> b5 = factory->creerBateau();
     b5->setY(4);
-    carteIA->ajouterBateau(b5);
+    //carteIA->ajouterBateau(b5);
 
-    factory = shared_ptr<FactoryCanard>(new FactoryCanard);
     shared_ptr<Bateau> b6 = factory->creerBateau();
     b6->setY(5);
-    carteIA->ajouterBateau(b6);
+    //carteIA->ajouterBateau(b6);
 
     factory = shared_ptr<FactoryCaneton>(new FactoryCaneton);
     shared_ptr<Bateau> b7 = factory->creerBateau();
     b7->setY(6);
-    carteIA->ajouterBateau(b7);
+    //carteIA->ajouterBateau(b7);
 
-    factory = shared_ptr<FactoryCaneton>(new FactoryCaneton);
     shared_ptr<Bateau> b8 = factory->creerBateau();
     b8->setY(7);
-    carteIA->ajouterBateau(b8);
+    //carteIA->ajouterBateau(b8);
 
-    factory = shared_ptr<FactoryCaneton>(new FactoryCaneton);
     shared_ptr<Bateau> b9 = factory->creerBateau();
     b9->setY(8);
-    carteIA->ajouterBateau(b9);
+    //carteIA->ajouterBateau(b9);
 
-    factory = shared_ptr<FactoryCaneton>(new FactoryCaneton);
     shared_ptr<Bateau> b10 = factory->creerBateau();
     b10->setY(9);
-    carteIA->ajouterBateau(b10);
+    //carteIA->ajouterBateau(b10);
+
+    QVector<shared_ptr<Bateau> > bs;
+    bs.push_back(b1);
+    bs.push_back(b2);
+    bs.push_back(b3);
+    bs.push_back(b4);
+    bs.push_back(b5);
+    bs.push_back(b6);
+    bs.push_back(b7);
+    bs.push_back(b8);
+    bs.push_back(b9);
+    bs.push_back(b10);
+    int i = 0;
+
+    do{
+      bs.at(i)->setX(rand() % 10);
+      bs.at(i)->setY(rand() % 10);
+      if((rand() % 2) == 0){
+          bs.at(i)->setHorizontal(true);
+      }
+      if(carteIA->ajouterBateau(bs.at(i))){
+          ++i;
+      }
+    } while(i != 10);
 
     aff_->getCore()->getTeam2()->setCarteInit(carteIA);
     aff_->getCore()->getTeam2()->setCarteJeu(carteIA);
@@ -100,8 +118,7 @@ void Mode1vsIA::jouer(){
 
         if(cases.at(caseAlea)->getContent()->touche()){
             for(shared_ptr<Bateau> b : bxT1){
-                if(b->estCoule()){
-                    // problème ici quand deux bateaux sont coulés de suite
+                if(b->estCoule() && b == cases.at(caseAlea)->getContent()){ //Si on tue 2 bateaux à suivre on va vouloir enleverBateau(b) d'un bateau déjà enlevé dans le meme appel de la fonction (sans reprendre le tableau des bateaux en vie)
                     aff_->getCore()->getTeam1()->getCarteJeu()->enleverBateau(b);
                 }
             }
